15e0bfcfcc6387d295f1ab55660f2a92c5b1b00c
AEGRA <aegra.code.guild@gmail.com>
2025-12-27 02:02:18 -0400

    Removendo arquivos duplicados da raiz

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# setup.sh
-mkdir -p data/raw data/processed scripts notebooks figures output docs tests
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
